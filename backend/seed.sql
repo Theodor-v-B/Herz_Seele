5bc8c1f4da7343643cce7afc7094c8386cb53643
Theodor-v-B <tgvonbrentano@hotmail.com>
2025-12-19 14:03:26 +0100

    edited seed.sql

--- backend/seed.sql
+++ backend/seed.sql
@@ -3,54 +3,67 @@ INSERT INTO anlaufstellen (stadt, plz, name, strasse, telefon) VALUES
 ('Berlin','10115','Berliner Krisendienste','Berliner Allee 26, 13088 Berlin','0800 111 0 111'),
 ('Berlin','10115','Charité – Ambulante Psychiatrie','Charitéplatz 1, 10117 Berlin','030 450 517002'),
 ('Berlin','10115','Caritas / Diakonie Berlin','Residenzstraße 90, 13409 Berlin','030 66633-0'),
+
 ('Dortmund','44135','Sozialpsychiatrischer Dienst Dortmund','Hoher Wall 9–11, 44137 Dortmund','0231 50-22534'),
 ('Dortmund','44135','Klinikum Dortmund – Psychiatrische Ambulanzen','Beurhausstraße 40, 44137 Dortmund','0231 953-0'),
 ('Dortmund','44135','Caritas Dortmund – Psychosoziale Beratung','Propsteihof 10, 44137 Dortmund','0231 1848-0'),
 ('Dortmund','44135','Diakonie Dortmund','Lange Straße 42, 44137 Dortmund','0231 8494-0'),
+
 ('Hamburg','20095','Sozialpsychiatrischer Dienst Hamburg','Billstraße 80, 20539 Hamburg','040 42828-0'),
 ('Hamburg','20095','Asklepios Kliniken – Psychiatrische Ambulanzen','Rübenkamp 220, 22307 Hamburg','040 1818-0'),
 ('Hamburg','20095','Universitätsklinikum Hamburg-Eppendorf (UKE)','Martinistraße 52, 20246 Hamburg','040 7410-0'),
 ('Hamburg','20095','Diakonie Hamburg','Königstraße 54, 22767 Hamburg','040 30620-0'),
+
 ('Stuttgart','70173','Sozialpsychiatrischer Dienst Stuttgart','Sophienstraße 1, 70178 Stuttgart','0711 216-59300'),
 ('Stuttgart','70173','Klinikum Stuttgart – Zentrum für Seelische Gesundheit','Prießnitzweg 24, 70374 Stuttgart','0711 278-0'),
 ('Stuttgart','70173','Caritas Stuttgart – Psychosoziale Beratung','Strombergstraße 11, 70188 Stuttgart','0711 2633-0'),
 ('Stuttgart','70173','Evangelische Gesellschaft Stuttgart','Büchsenstraße 34/36, 70174 Stuttgart','0711 2054-0'),
+
 ('München','80331','Sozialpsychiatrischer Dienst München','Paul-Heyse-Straße 20, 80336 München','089 233-47234'),
 ('München','80331','LMU Klinikum – Psychiatrische Ambulanz','Nußbaumstraße 7, 80336 München','089 4400-55511'),
 ('München','80331','Caritas München','Hirtenstraße 4, 80335 München','089 55169-0'),
 ('München','80331','Isar-Amper-Klinikum (ambulant)','Ringstraße 8, 85540 Haar','089 4562-0'),
+
 ('Saarbrücken','66111','Sozialpsychiatrischer Dienst Saarbrücken','Stengelstraße 10–12, 66117 Saarbrücken','0681 506-5332'),
 ('Saarbrücken','66111','Klinikum Saarbrücken – Psychiatrie','Winterberg 1, 66119 Saarbrücken','0681 963-0'),
 ('Saarbrücken','66111','Caritas Saarbrücken','Ursulinenstraße 67, 66111 Saarbrücken','0681 30906-0'),
 ('Saarbrücken','66111','Diakonie Saarbrücken','Neumünsterstraße 10, 66111 Saarbrücken','0681 940440'),
+
 ('Mainz','55116','Sozialpsychiatrischer Dienst Mainz','Kaiserstraße 3–5, 55116 Mainz','06131 12-3300'),
 ('Mainz','55116','Universitätsmedizin Mainz – Psychiatrie','Untere Zahlbacher Straße 8, 55131 Mainz','06131 17-0'),
 ('Mainz','55116','Caritas Mainz','Bahnstraße 32, 55128 Mainz','06131 2848-0'),
 ('Mainz','55116','Diakonie Mainz','Kaiserstraße 37, 55116 Mainz','06131 25090'),
+
 ('Wiesbaden','65183','Sozialpsychiatrischer Dienst Wiesbaden','Konradinerallee 11, 65189 Wiesbaden','0611 31-4600'),
 ('Wiesbaden','65183','Helios Dr. Horst-Schmidt-Kliniken','Ludwig-Erhard-Straße 100, 65199 Wiesbaden','0611 43-0'),
 ('Wiesbaden','65183','Caritas Wiesbaden','Bahnhofstraße 26, 65185 Wiesbaden','0611 1749-0'),
 ('Wiesbaden','65183','Diakonie Wiesbaden','Rheinstraße 65, 65185 Wiesbaden','0611 36091-0'),
+
 ('Erfurt','99084','Sozialpsychiatrischer Dienst Erfurt','Juri-Gagarin-Ring 150, 99084 Erfurt','0361 655-4200'),
 ('Erfurt','99084','Helios Klinikum Erfurt – Psychiatrie','Nordhäuser Straße 74, 99089 Erfurt','0361 781-0'),
 ('Erfurt','99084','Caritas Erfurt','Regierungsstraße 44, 99084 Erfurt','0361 6574-0'),
 ('Erfurt','99084','Diakonie Thüringen','Löberstraße 23, 99084 Erfurt','0361 51800-0'),
+
 ('Dresden','01067','Sozialpsychiatrischer Dienst Dresden','Theaterstraße 11, 01067 Dresden','0351 488-5301'),
 ('Dresden','01067','Universitätsklinikum Carl Gustav Carus','Fetscherstraße 74, 01307 Dresden','0351 458-0'),
 ('Dresden','01067','Caritas Dresden','Budapester Straße 15, 01069 Dresden','0351 4988-0'),
 ('Dresden','01067','Diakonie Dresden','Glashütter Straße 101a, 01277 Dresden','0351 31001-0'),
+
 ('Magdeburg','39104','Sozialpsychiatrischer Dienst Magdeburg','Lübecker Straße 32, 39124 Magdeburg','0391 540-6000'),
 ('Magdeburg','39104','Universitätsklinikum Magdeburg – Psychiatrie','Leipziger Straße 44, 39120 Magdeburg','0391 67-0'),
 ('Magdeburg','39104','Caritas Magdeburg','Karl-Schmidt-Straße 5c, 39104 Magdeburg','0391 40851-0'),
 ('Magdeburg','39104','Diakonie Magdeburg','Lübecker Straße 108, 39124 Magdeburg','0391 24451-0'),
+
 ('Potsdam','14467','Sozialpsychiatrischer Dienst Potsdam','Friedrich-Ebert-Straße 79/81, 14467 Potsdam','0331 289-2200'),
 ('Potsdam','14467','Klinikum Ernst von Bergmann','Charlottenstraße 72, 14467 Potsdam','0331 241-0'),
 ('Potsdam','14467','Caritas Potsdam','Althoffstraße 2, 14469 Potsdam','0331 710298'),
 ('Potsdam','14467','Diakonie Potsdam','Rudolf-Breitscheid-Straße 24, 14482 Potsdam','0331 7405-0'),
+
 ('Hannover','30159','Sozialpsychiatrischer Dienst Hannover','Podbielskistraße 168, 30177 Hannover','0511 616-4600'),
 ('Hannover','30159','Medizinische Hochschule Hannover (MHH)','Carl-Neuberg-Straße 1, 30625 Hannover','0511 532-0'),
 ('Hannover','30159','KRH Kliniken – Psychiatrie','Rudolf-Virchow-Straße 5, 30625 Hannover','0511 906-0'),
 ('Hannover','30159','Diakonie Hannover','Ebhardtstraße 3A, 30159 Hannover','0511 3687-0'),
+
 ('Schwerin','19053','Sozialpsychiatrischer Dienst Schwerin','Am Packhof 2–6, 19053 Schwerin','0385 545-2900'),
 ('Schwerin','19053','Helios Kliniken Schwerin – Psychiatrie','Wismarsche Straße 393–397, 19055 Schwerin','0385 520-0'),
 ('Schwerin','19053','Caritas Schwerin','Am Grünen Tal 50, 19063 Schwerin','0385 59179-0'),
