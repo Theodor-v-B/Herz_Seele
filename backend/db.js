cfd29f4155fe1a30a31b27df7acd34da17861f57
Theodor-v-B <tgvonbrentano@hotmail.com>
2026-01-07 10:14:28 +0100

    changed db.js password

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
     host: process.env.PGHOST || "postgres",
     user: process.env.PGUSER || "postgres",
-    password: process.env.PGPASSWORD || "password",
+    password: process.env.PGPASSWORD || "password123",
     database: process.env.PGDATABASE || "anlaufstellen",
     post: Number(process.env.PGPORT || 5432),
 });
